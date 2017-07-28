dc930b6f91216df638bd2b21ea4b4ac5fb146970
Chris Snowden <cdsnowden@gmail.com>
2017-07-28 13:09:10 +0100

    Setting fn up

--- scrub.js
+++ scrub.js
@@ -1,4 +1,56 @@
-/* jQuery */
+/* Scrubber Slider */
+  /* vanillaJS */
+
+function Scrub(el) {
+   
+  // scrubber slider main action fn
+  function mover(pos,full) {
+    let shrink = full - pos;
+    let sliding = document.getElementsByClassName('sliding')[0];
+       sliding.setAttribute("style", "left:" + pos + "px"); 
+    let contentLeft = document.getElementsByClassName('content-left')[0];
+       contentLeft.setAttribute("style", "width:" + pos + "px");
+    let contentRight = document.getElementsByClassName('content-right')[0];
+       contentRight.setAttribute("style", "width:" + shrink + "px"); 
+  }
+  
+  // get chosen slider container
+  // checking id > class > attr name
+  if ( document.getElementById(el) != null ) {
+    const scrubSlider = document.getElementById(el)[0];
+  }
+  else if ( document.getElementsByClassName(el) != null ) {
+    const scrubSlider = document.getElementsByClassName(el)[0];
+  }
+  else {    
+    const scrubSlider = document.getElementsByName(el)[0];
+  }
+  // get chosen slider's children(images)
+  
+  
+  // add scrubber control/handle
+  const scrubberHandle = document.createElement('div');
+  scrubberHandle.className="sliding";
+  scrubberHandle.innerHTML = '<span class="sliding-left"></span><span class="sliding-right"></span>';
+  scrubSlider.appendChild(scrubberHandle); 
+   
+  // add mousemove listener
+  scrubSlider.addEventListener('mousemove', function(e) {
+    let mousePosition = e.clientX;
+    let fullWidth = cont.offsetWidth;
+    mover(mousePosition,fullWidth); 
+  });
+  
+}
+
+
+
+
+
+
+
+
+/* jQuery version - incomplete */
 
 // function mover(pos,full,shrink) {
 //     $('.sliding').css({
@@ -17,22 +69,3 @@
 // fullWidth = $(".box-itself").width();
 // mover(mousePosition,fullWidth);  
 // });
-
-
-/* vanillaJS */
-
-function mover(pos,full) {
-  let shrink = full - pos;
-  let sliding = document.getElementsByClassName('sliding')[0];
-     sliding.setAttribute("style", "left:" + pos + "px"); 
-  let contentLeft = document.getElementsByClassName('content-left')[0];
-     contentLeft.setAttribute("style", "width:" + pos + "px");
-  let contentRight = document.getElementsByClassName('content-right')[0];
-     contentRight.setAttribute("style", "width:" + shrink + "px"); 
-}
-const cont = document.getElementsByClassName('box-itself')[0];
-cont.addEventListener('mousemove', function(e) {
-    let mousePosition = e.clientX;
-    let fullWidth = cont.offsetWidth;
-    mover(mousePosition,fullWidth); 
-});
