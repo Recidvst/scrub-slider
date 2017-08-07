0dd9949a9b3c00c7bbf02365661fe2862a342503
Chris Snowden <cdsnowden@gmail.com>
2017-08-07 14:53:16 +0100

    Changing method of injection

--- js/scrub.js
+++ js/scrub.js
@@ -21,8 +21,37 @@ function Scrub(el) {
 
       // get chosen slider's images
         const scrubChildren = document.getElementsByClassName('scrub-slider')[0].children;
-
-      // wrap in scrub html
+       
+     function createScrubImages(el,type,index) {
+        if ( type == 'DIV' ) {
+           let scrubImage = document.createElement('div');
+           scrubImage.className= "scrub-content" +  ( i == 0 ) ? "scrub-left" : "scrub-right";
+           scrubImage.appendChild( ( i == 0 ) ? scrubChildren[0] : scrubChildren[1] );
+           if ( el.style.backgroundImage == '' ) {
+            // warning no image
+           }
+        }
+        else if ( type == 'IMG' ) {
+           let scrubImage = document.createElement('div');
+           let imgSrc = el.getAttribute('src');
+           scrubImage.className= "scrub-content" +  ( i == 0 ) ? "scrub-left" : "scrub-right";
+           scrubImage.appendChild( document.createElement('div') );
+           scrubImage.children[0].style.backgroundImage = 'url(' + imgSrc + ')';
+        }
+        else {
+        }       
+      scrubSlider.appendChild(scrubImage);
+     }
+    
+    for(i=0; i < 2; i++) {
+      let child = scrubChildren[i];
+      if ( scrubChildren[i] && child.tagName ) {
+        createScrubImages( scrubChildren[i], child.tagName, i );
+      }
+    }
+    
+      // wrap in scrub html 
+      /*
       var scrubLeft = document.createElement('div');
       scrubLeft.className= "scrub-content scrub-left";
       scrubLeft.appendChild((scrubChildren[0]) ? scrubChildren[0] : '<img src="" title="Missing image" />');
@@ -30,11 +59,12 @@ function Scrub(el) {
       scrubRight.className= "scrub-content scrub-right";
       scrubRight.appendChild((scrubChildren[1]) ? scrubChildren[1] : '<img src="" title="Missing image" />');
       // scrubRight.innerHTML = (scrubChildren[1]) ? scrubChildren[1] : '<img src="" title="Missing image" />';
-
+      */
+    
       // inject
       scrubSlider.innerHTML = '';
-      scrubSlider.appendChild(scrubLeft);
-      scrubSlider.appendChild(scrubRight);
+//       scrubSlider.appendChild(scrubLeft);
+//       scrubSlider.appendChild(scrubRight);
 
       callback(scrubSlider);
   }
@@ -50,11 +80,14 @@ function Scrub(el) {
     function mover(pos,full) {
       let shrink = full - pos;
       let sliding = document.getElementsByClassName('sliding')[0];
-         sliding.setAttribute("style", "left:" + pos + "px");
+//          sliding.setAttribute("style", "left:" + pos + "px");
+         sliding.style.left = ((pos > 0) ? pos : 0) + "px";
       let contentLeft = document.getElementsByClassName('scrub-left')[0];
-         contentLeft.setAttribute("style", "width:" + ((pos > 0) ? pos : 0) + "px");
+//          contentLeft.setAttribute("style", "width:" + ((pos > 0) ? pos : 0) + "px");
+         contentLeft.style.width = ((pos > 0) ? pos : 0) + "px";
       let contentRight = document.getElementsByClassName('scrub-right')[0];
-         contentRight.setAttribute("style", "width:" + ((shrink > 0) ? shrink : 0) + "px");
+//          contentRight.setAttribute("style", "width:" + ((shrink > 0) ? shrink : 0) + "px");      
+         contentRight.style.width = ((shrink > 0) ? shrink : 0) + "px";
     }
 
     // add mousemove listener
