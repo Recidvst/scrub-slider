0d769bd12cada8ba126fa673e4e85b7546ec7b11
Chris Snowden <cdsnowden@gmail.com>
2017-08-09 00:51:02 +0100

    handle multiple sliders | better js | starting page

--- js/example.js
+++ js/example.js
@@ -1,1 +1,7 @@
-Scrub('example-slider');
+Scrub('#slider1');
+
+Scrub('#slider2');
+
+Scrub('.slider3');
+
+Scrub('nonexistent');
--- js/scrub.js
+++ js/scrub.js
@@ -2,127 +2,91 @@
   /* vanillaJS */
 
 function Scrub(el) {
-
-  function utilityFn(callback) {
-
+try{
+    function utilityFn(callback) {
       // get chosen slider container
-      // checking id > class > attr name
       var scrubSlider;
-      if ( document.getElementById(el) != null ) {
-        scrubSlider = document.getElementById(el)[0];
+      scrubSlider = document.querySelectorAll(el)[0];
+      
+      // avoid non-specific classes..
+      if ( el.indexOf('.') > -1 && document.querySelectorAll(el).length > 1 ) {
+        console.warn('%cScrubber Slider works best if you use an %cID%c or a %cunique%c class... ','color:cornflowerblue;','color:indianred;','color:cornflowerblue;','color:indianred;','color:cornflowerblue;');
       }
-      else if ( document.getElementsByClassName(el) != null ) {
-        scrubSlider = document.getElementsByClassName(el)[0];
-      }
-      else {
-        scrubSlider = document.getElementsByName(el)[0];
-      }
-      scrubSlider.className += " scrub-slider";
 
-      // get chosen slider's images
-        const scrubChildren = document.getElementsByClassName('scrub-slider')[0].children;
-       
-     function createScrubImages(el,type,index) {
-        if ( type == 'DIV' ) {
-           let scrubImage = document.createElement('div');
-           scrubImage.className= "scrub-content" +  ( i == 0 ) ? "scrub-left" : "scrub-right";
-           scrubImage.appendChild( ( i == 0 ) ? scrubChildren[0] : scrubChildren[1] );
-           if ( el.style.backgroundImage == '' ) {
-            // warning no image
-           }
+        if ( scrubSlider != undefined ) {
+          scrubSlider.className += " scrub-slider";
+
+            // get chosen slider's images
+            var scrubChildren = scrubSlider.children;
+
+            function createScrubImages(el,type,index) {
+                if ( type == 'DIV' ) {
+                   // clone div
+                   let scrubImage = el.cloneNode(true);
+                   el.parentNode.removeChild(el);
+                   // inject scrub container
+                   let scrubCont = document.createElement('div');
+                   scrubCont.className = ( index == 0 ) ? "scrub-content scrub-left" : "scrub-content scrub-right";
+                   scrubSlider.insertBefore(scrubCont,scrubSlider.firstChild);
+                   // re-attach div
+                   scrubCont.append(scrubImage);
+                   // need a background image..
+                   if ( el.style.backgroundImage == '' ) {
+                    console.warn('%cScrubber Slider divs must have a %cbackground image%c >:[','color:cornflowerblue;','color:indianred;','color:cornflowerblue;');
+                   }
+                }
+                else if ( type == 'IMG' ) {
+                   // clone div
+                   let scrubImage = document.createElement('div');
+                   let imgSrc = el.getAttribute('src');
+                   el.parentNode.removeChild(el);
+                   // inject scrub container
+                   let scrubCont = document.createElement('div');
+                   scrubCont.className = ( index == 0 ) ? "scrub-content scrub-left" : "scrub-content scrub-right";
+                   scrubSlider.insertBefore(scrubCont,scrubSlider.firstChild);
+                   // re-attach div
+                   scrubImage.style.backgroundImage = 'url(' + imgSrc + ')';
+                   scrubCont.append(scrubImage);
+                }
+            }
+            for(let i=0; i < 2; i++) {
+              let child = scrubChildren[i];
+              if ( scrubChildren[i] && child.tagName ) {
+                createScrubImages( scrubChildren[i], child.tagName, i );
+              }
+            }
+
+        callback(scrubSlider);
         }
-        else if ( type == 'IMG' ) {
-           let scrubImage = document.createElement('div');
-           let imgSrc = el.getAttribute('src');
-           scrubImage.className= "scrub-content" +  ( i == 0 ) ? "scrub-left" : "scrub-right";
-           scrubImage.appendChild( document.createElement('div') );
-           scrubImage.children[0].style.backgroundImage = 'url(' + imgSrc + ')';
-        }
-        else {
-        }       
-      scrubSlider.appendChild(scrubImage);
-     }
-    
-    for(i=0; i < 2; i++) {
-      let child = scrubChildren[i];
-      if ( scrubChildren[i] && child.tagName ) {
-        createScrubImages( scrubChildren[i], child.tagName, i );
-      }
-    }
-    
-      // wrap in scrub html 
-      /*
-      var scrubLeft = document.createElement('div');
-      scrubLeft.className= "scrub-content scrub-left";
-      scrubLeft.appendChild((scrubChildren[0]) ? scrubChildren[0] : '<img src="" title="Missing image" />');
-      var scrubRight = document.createElement('div');
-      scrubRight.className= "scrub-content scrub-right";
-      scrubRight.appendChild((scrubChildren[1]) ? scrubChildren[1] : '<img src="" title="Missing image" />');
-      // scrubRight.innerHTML = (scrubChildren[1]) ? scrubChildren[1] : '<img src="" title="Missing image" />';
-      */
-    
-      // inject
-      scrubSlider.innerHTML = '';
-//       scrubSlider.appendChild(scrubLeft);
-//       scrubSlider.appendChild(scrubRight);
-
-      callback(scrubSlider);
-  }
-
-  utilityFn(function(scrubSlider) {
-    // add scrubber control/handle
-    const scrubberHandle = document.createElement('div');
-    scrubberHandle.className= "sliding";
-    scrubberHandle.innerHTML = '<span class="sliding-left"></span><span class="sliding-right"></span>';
-    scrubSlider.appendChild(scrubberHandle);
-
-    // scrubber slider main action fn
-    function mover(pos,full) {
-      let shrink = full - pos;
-      let sliding = document.getElementsByClassName('sliding')[0];
-//          sliding.setAttribute("style", "left:" + pos + "px");
-         sliding.style.left = ((pos > 0) ? pos : 0) + "px";
-      let contentLeft = document.getElementsByClassName('scrub-left')[0];
-//          contentLeft.setAttribute("style", "width:" + ((pos > 0) ? pos : 0) + "px");
-         contentLeft.style.width = ((pos > 0) ? pos : 0) + "px";
-      let contentRight = document.getElementsByClassName('scrub-right')[0];
-//          contentRight.setAttribute("style", "width:" + ((shrink > 0) ? shrink : 0) + "px");      
-         contentRight.style.width = ((shrink > 0) ? shrink : 0) + "px";
     }
 
-    // add mousemove listener
-    scrubSlider.addEventListener('mousemove', function(e) {
-      let mousePosition = e.clientX;
-      let fullWidth = scrubSlider.offsetWidth;
-      mover(mousePosition,fullWidth);
-    });
-  });
+        utilityFn(function(scrubSlider) {
+            // add scrubber control/handle
+            const scrubberHandle = document.createElement('div');
+            scrubberHandle.className= "sliding";
+            scrubberHandle.innerHTML = '<span class="sliding-left"></span><span class="sliding-right"></span>';
+            scrubSlider.appendChild(scrubberHandle);
 
+            // scrubber slider main action fn
+            function mover(pos,full,slider) {
+                if ( scrubberHandle ) {
+                    let shrink = full - pos;
+                    let sliding = slider.querySelectorAll('.sliding');
+                       sliding[0].style.left = ((pos > 0) ? pos : 0) + "px";
+                    let contentLeft = slider.querySelectorAll('.scrub-left');
+                       contentLeft[0].style.width = ((pos > 0) ? pos : 0) + "px";
+                    let contentRight = slider.querySelectorAll('.scrub-right');
+                       contentRight[0].style.width = ((shrink > 0) ? shrink : 0) + "px";
+                }
+            }
+            // add mousemove listener
+            scrubSlider.addEventListener('mousemove', function(e) {
+              let mousePosition = e.clientX;
+              let fullWidth = scrubSlider.offsetWidth;
+              mover(mousePosition,fullWidth,scrubSlider);
+            });
+        });
+}catch(e){
+    console.error('uh-oh: ' + e);
 }
-
-
-
-
-
-
-
-
-/* jQuery version - incomplete */
-
-// function mover(pos,full,shrink) {
-//     $('.sliding').css({
-//       left : pos + 'px'
-//     });
-//     shrink = full - pos;
-//     $('.content-left').css({
-//       width : pos + 'px'
-//     });
-//     $('.content-right').css({
-//       width : shrink + 'px'
-//     });
-// }
-// $(".box-itself").mousemove(function(event) {
-// mousePosition = event.clientX;
-// fullWidth = $(".box-itself").width();
-// mover(mousePosition,fullWidth);
-// });
+}
